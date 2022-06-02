152c90ce8c98a5ee7e7353cc075fb693cb8b779e
huanmie <yonghenghuanmie@gmail.com>
2022-06-02 13:49:56 +0800

    Add: SellAsset strategy

--- binance.js
+++ binance.js
@@ -3,18 +3,28 @@
 *	@date 2022.5.25
 */
 
-const fs = require('fs');
-const axios = require('axios');
-const CryptoJS = require("crypto-js");
-const { Console } = require('console');
-const { Spot } = require('@binance/connector');
-
 const apiKey = 'eUNQo4d9VZXesfiIQp5jNGjqQ2VmxZw1XI1ySln6HBBn1aAfROCU5oXdjwYjouFG';
 const apiSecret = fs.readFileSync(".secret").toString().trim();
 
 const output = fs.createWriteStream('./output.log', { flags: "a" });
 const errorOutput = fs.createWriteStream('./error.log', { flags: "a" });
 const logger = new Console({ stdout: output, stderr: errorOutput });
+
+const frequency = 1000;
+const client = new Spot(apiKey, apiSecret, { logger: logger });
+// KeepFutureAlive
+const balance_tolerance = 0.05;
+const quote_asset = "USDT";
+const base_asset = "ETH";
+const quote_need = 250;
+const down_need = 200;
+const base_down_asset = base_asset + "DOWN";
+// SellAsset
+const future_count = 1000;
+const future_count_usd = 10;
+const start_price = 2325;
+const profit_percent = 0.05;
+
 
 async function GetBalance(client, asset) {
 	let response = await client.account();
@@ -95,52 +105,48 @@ function GetMarketBuyQuantity(response) {
 	return quantity;
 }
 
-async function KeepFutureAlive(data) {
-	const balance_tolerance = 0.05;
-	const quote_asset = "USDT";
-	const base_asset = "ETH";
-	const quote_need = 500;
-	const down_need = 400;
-	const base_down_asset = base_asset + "DOWN";
-
-	let future_name = GetFutureName(base_asset);
-	let future_balance = await GetFutureBalance(future_name);
-	if (future_balance <= balance_tolerance) {
-		let future_price = await GetFuturePrice(future_name);
+async function KeepFutureAlive(future_data, persistence_data) {
+	if (future_data.future_balance <= balance_tolerance) {
 		logger.log("================================================================================================================================");
-		logger.log(new Date().toString() + " future_price:" + future_price + " future_balance:" + future_balance);
+		logger.log(new Date().toString() + " future_price:" + future_data.future_price + " future_balance:" + future_data.future_balance);
 
-		let locked_balance = await GetLockedBalance(data.client, base_asset);
+		let locked_balance = await GetLockedBalance(client, base_asset);
 		if (locked_balance > 0) {
 			logger.log(new Date().toString() + " CANCEL all the " + base_asset + quote_asset + " orders");
-			await data.client.cancelOpenOrders(base_asset + quote_asset);
+			await client.cancelOpenOrders(base_asset + quote_asset);
 		}
 
 		let base_transfer_quantity = 0;
-		let base_balance = await GetBalance(data.client, base_asset);
-		if (base_balance > 0.1 * quote_need / future_price) {
+		let base_balance = await GetBalance(client, base_asset);
+		if (base_balance > 0.1 * quote_need / future_data.future_price) {
 			base_transfer_quantity = base_balance;
 		}
 		else {
-			let quote_balance = await GetBalance(data.client, quote_asset);
-			let down_balance = await GetBalance(data.client, base_down_asset);
-			if (quote_balance > quote_need) {
+			let quote_balance = await GetBalance(client, quote_asset);
+			let down_balance = await GetBalance(client, base_down_asset);
+			let last_sold_quantity = quote_need;
+			if (persistence_data.sold_quantity.length != 0)
+				last_sold_quantity = persistence_data.sold_quantity[persistence_data.sold_quantity.length - 1];
+
+			if (quote_balance > last_sold_quantity) {
 				logger.log(new Date().toString() + " quote_balance:" + quote_balance);
-				const response = await data.client.newOrder(base_asset + quote_asset, "BUY", "MARKET", { quoteOrderQty: quote_need });
+				const response = await client.newOrder(base_asset + quote_asset, "BUY", "MARKET", { quoteOrderQty: last_sold_quantity });
 				base_transfer_quantity = GetMarketBuyQuantity(response);
-				data.purchase_price.push(response.data.fills[0].price);
-				data.purchase_quantity.push(base_transfer_quantity);
-				logger.log(new Date().toString() + " USE " + quote_need + " " + quote_asset + " to BUY " + base_transfer_quantity + " " + base_asset);
+				persistence_data.purchase_price.push(response.data.fills[0].price);
+				persistence_data.purchase_quantity.push(base_transfer_quantity);
+				if (persistence_data.sold_quantity.length != 0)
+					persistence_data.sold_quantity.pop();
+				logger.log(new Date().toString() + " USE " + last_sold_quantity + " " + quote_asset + " to BUY " + base_transfer_quantity + " " + base_asset);
 
 			} else if (down_balance > down_need) {
 				logger.log(new Date().toString() + " down_balance:" + down_balance);
-				let quote_quantity = GetMarketSellQuantity(await data.client.newOrder(base_down_asset + quote_asset, "SELL", "MARKET", { quantity: down_need }));
+				let quote_quantity = GetMarketSellQuantity(await client.newOrder(base_down_asset + quote_asset, "SELL", "MARKET", { quantity: down_need }));
 				logger.log(new Date().toString() + " USE " + down_need + " " + base_down_asset + " to SELL " + quote_quantity + " " + quote_asset);
 
-				const response = await data.client.newOrder(base_asset + quote_asset, "BUY", "MARKET", { quoteOrderQty: quote_quantity });
+				const response = await client.newOrder(base_asset + quote_asset, "BUY", "MARKET", { quoteOrderQty: quote_quantity });
 				base_transfer_quantity = GetMarketBuyQuantity(response);
-				data.purchase_price.push(response.data.fills[0].price);
-				data.purchase_quantity.push(base_transfer_quantity);
+				persistence_data.purchase_price.push(response.data.fills[0].price);
+				persistence_data.purchase_quantity.push(base_transfer_quantity);
 				logger.log(new Date().toString() + " USE " + quote_quantity + " " + quote_asset + " to BUY " + base_transfer_quantity + " " + base_asset);
 
 			} else {
@@ -149,28 +155,107 @@ async function KeepFutureAlive(data) {
 		}
 
 		if (base_transfer_quantity > 0) {
-			await data.client.futuresTransfer(base_asset, base_transfer_quantity, 3);
-			logger.log(new Date().toString() + " Transfer " + base_transfer_quantity + " " + base_asset + " to Future " + future_name);
-			data.wait_time *= 5;
+			await client.futuresTransfer(base_asset, base_transfer_quantity, 3);
+			logger.log(new Date().toString() + " Transfer " + base_transfer_quantity + " " + base_asset + " to Future " + future_data.future_name);
+			future_data.wait_time = frequency * 5;
+			return true;
 		}
 	}
+	return false;
+}
+
+function CalculateFutureProfit(future_data, persistence_data) {
+	if (future_data.future_price > persistence_data.cost_price)
+		return ((1 / start_price - 1 / future_data.future_price) - (1 / start_price - 1 / persistence_data.cost_price)) * future_count * future_count_usd;
+	else
+		return 0;
+}
+
+async function SellAsset(future_data, persistence_data) {
+	let target_price;
+	let sell_quantity;
+	if (persistence_data.sell_price.length != 0) {
+		target_price = persistence_data.sell_price[persistence_data.sell_price.length - 1];
+		sell_quantity = persistence_data.purchase_quantity[persistence_data.purchase_quantity.length - 1];
+	} else if (persistence_data.purchase_quantity.length != 0) {
+		target_price = persistence_data.purchase_price[persistence_data.purchase_price.length - 1] * (1 + profit_percent);
+		sell_quantity = persistence_data.purchase_quantity[persistence_data.purchase_quantity.length - 1];
+	} else {
+		target_price = persistence_data.cost_price * (1 + profit_percent);
+		sell_quantity = CalculateFutureProfit(future_data, persistence_data);
+	}
+
+	if (future_data.future_price > target_price) {
+		logger.log("================================================================================================================================");
+		logger.log(new Date().toString() + " future_price:" + future_data.future_price + " future_balance:" + future_data.future_balance);
+
+		await client.futuresTransfer(base_asset, sell_quantity, 4);
+		logger.log(new Date().toString() + " Transfer " + sell_quantity + " " + base_asset + " to Spot " + base_asset + quote_asset);
+		const quote_quantity = GetMarketSellQuantity(await client.newOrder(base_asset + quote_asset, "SELL", "MARKET", { quantity: sell_quantity }));
+		persistence_data.sold_quantity.push(quote_quantity);
+		if (persistence_data.sell_price.length != 0) {
+			persistence_data.sell_price.pop();
+			persistence_data.purchase_price.pop();
+			persistence_data.purchase_quantity.pop();
+		} else if (persistence_data.purchase_quantity.length != 0) {
+			persistence_data.purchase_price.pop();
+			persistence_data.purchase_quantity.pop();
+		} else {
+			persistence_data.cost_price = future_data.future_price;
+		}
+		logger.log(new Date().toString() + " USE " + sell_quantity + " " + base_asset + " to SELL " + quote_quantity + " " + quote_asset);
+		return true;
+	}
+	return false;
 }
 
 (async function main() {
-	const client = new Spot(apiKey, apiSecret, { logger: logger });
-	const frequency = 1000;
-	let purchase_price = [];
-	let purchase_quantity = [];
+	let cost_price = 2255;
+	let purchase_price = [2031, 1880, 1835];
+	let purchase_quantity = [0.2461, 0.2659, 0.2723];
+	// overwrite purchase_price sell rule.
+	let sell_price = [2195, 2095, 1995];
+	let sold_quantity = [];
+
+	let persistence_data = {
+		cost_price: cost_price,
+		purchase_price: purchase_price,
+		purchase_quantity: purchase_quantity,
+		sell_price: sell_price,
+		sold_quantity: sold_quantity
+	};
+
+	let persistence_data_json = JSON.stringify(persistence_data);
+	try {
+		const trade_data = fs.readFileSync("trade_data.json").toString();
+		persistence_data_json = trade_data;
+	} catch (error) { }
+	if (persistence_data.purchase_price.length != persistence_data.purchase_quantity.length ||
+		persistence_data.purchase_quantity.length < persistence_data.sell_price.length) {
+		logger.error(new Date().toString() + " purchase_price.length must equal to purchase_quantity.length and their length greater than sell_price.length.");
+		return;
+	}
+	const future_name = GetFutureName(base_asset);
 
 	while (true) {
-		let data = { client: client, wait_time: frequency, purchase_price: purchase_price, purchase_quantity: purchase_quantity };
+		const future_balance = await GetFutureBalance(future_name);
+		const future_price = await GetFuturePrice(future_name);
+		let future_data = { future_name: future_name, future_balance: future_balance, future_price: future_price, wait_time: frequency };
 		try {
-			await KeepFutureAlive(data);
+			if (await KeepFutureAlive(future_data, persistence_data)) {
+				persistence_data_json = JSON.stringify(persistence_data);
+				fs.writeFileSync("trade_data.json", persistence_data_json);
+			}
+			if (await SellAsset(future_data, persistence_data)) {
+				persistence_data_json = JSON.stringify(persistence_data);
+				fs.writeFileSync("trade_data.json", persistence_data_json);
+			}
 		}
 		catch (error) {
+			persistence_data = JSON.parse(persistence_data_json);
 			logger.error(new Date().toString() + " " + error);
 			continue;
 		}
-		await new Promise(resolve => setTimeout(resolve, data.wait_time));
+		await new Promise(resolve => setTimeout(resolve, future_data.wait_time));
 	}
 })();
